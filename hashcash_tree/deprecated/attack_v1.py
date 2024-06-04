7735e45556d578e532691d6974413d0435d4d933
malvi <mario.alviano@gmail.com>
2024-06-04 16:06:45 +0200

    parallel computation of v1

--- hashcash_tree/deprecated/attack_v1.py
+++ hashcash_tree/deprecated/attack_v1.py
@@ -4,6 +4,7 @@ import typer
 from dumbo_utils.console import console
 
 from hashcash_tree.deprecated.primitives_v1 import HashcashTree, UINT
+from hashcash_tree.primitives import HashcashTree as NewHashcashTree
 
 PAYLOAD: Final = b"Attack the hashcash tree v1!"
 
@@ -12,9 +13,9 @@ def main(
         prefix_length: int = typer.Option(4, "--prefix-length", "-p", help="Number of zeros in hash values"),
         size: int = typer.Option(15, "--size", "-s", help="Number of nodes in the hashcash tree"),
         workers: int = typer.Option(1, "--workers", "-w", help="Number of concurrent jobs"),
-        with_check: bool = typer.Option(False, "--with-check", "-c", help="Check the hashcash tree")
+        with_check: bool = typer.Option(False, "--with-check", "-c", help="Check the hashcash tree"),
 ):
-    hashcash_tree = HashcashTree.of(prefix_length=prefix_length, size=UINT(size))
+    hashcash_tree = HashcashTree.of(prefix_length=prefix_length, size=UINT(size)) if workers >= 0 else NewHashcashTree.of(prefix_length=prefix_length, size=UINT(size))
     result = hashcash_tree.apply_parallel(PAYLOAD, workers=workers) if workers > 0 else hashcash_tree.apply(PAYLOAD)
     if with_check:
         assert hashcash_tree.verify(message=PAYLOAD, leaf_index=1,
